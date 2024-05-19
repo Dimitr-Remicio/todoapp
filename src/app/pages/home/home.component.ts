f766abd6b144a6829e036ba9ee50ab44cf1c3f5e
Dimitr-remicio <dimitr.darm27@gmail.com>
2024-05-19 13:13:00 -0500

    adding counter of items, models for todo app

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,28 +1,72 @@
-import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
-
+import { CommonModule } from "@angular/common";
+import { Component, signal } from "@angular/core";
+import { Task } from "../../models/task.model";
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  templateUrl: "./home.component.html",
+  styleUrl: "./home.component.css",
 })
 export class HomeComponent {
-  tasks = signal([
-    'Read Documentation',
-    "Create a new project",
-    "Start to Code",
-    "Create all components",
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: "crear proyecto (using array)",
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: "Create all components",
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: "Start to Code",
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: "Create a new project",
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: "Read Documentation",
+      completed: true,
+    },
   ]);
   changeHandler(event: Event) {
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
-    this.tasks.update((tasks) => [...tasks, newTask]);
+    this.addTask(newTask);
     input.value = "";
-  };
+  }
 
-  deleteTask(index:number) {
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number) {
     this.tasks.update((tasks) => tasks.filter((_, i) => i !== index));
-  };
+  }
+
+  updateTask(index: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            completed: !task.completed,
+          };
+        }
+        return task;
+      });
+    });
+  }
 }
